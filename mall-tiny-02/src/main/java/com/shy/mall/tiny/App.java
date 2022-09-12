8a8e4b1506343de4599d8bb0b3167471a05e7755
Shan <1713944558@qq.com>
2022-09-12 13:33:47 +0800

    集成swgger

---
+++ mall-tiny-02/src/main/java/com/shy/mall/tiny/App.java
@@ -0,0 +1,16 @@
+package com.shy.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hongyuan.shan
+ * @date 2022/09/11 23:44
+ * @description
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ mall-tiny-02/src/main/java/com/shy/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,56 @@
+package com.shy.mall.tiny.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.shy.mall.tiny.mbg.mapper.PmsBrandMapper;
+import com.shy.mall.tiny.mbg.model.PmsBrand;
+import com.shy.mall.tiny.mbg.model.PmsBrandExample;
+import com.shy.mall.tiny.service.PmsBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author :shy
+ * @date : 2022/9/1 0:19
+ */
+@Service
+public class PmsBrandServiceImpl implements PmsBrandService {
+    @Autowired
+    private PmsBrandMapper pmsBrandMapper;
+
+    @Override
+    public List<PmsBrand> listAllBrand() {
+        return pmsBrandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public List<PmsBrand> listBrandByPage(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        return pmsBrandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrand brand) {
+        brand.setId(id);
+        return pmsBrandMapper.updateByPrimaryKeySelective(brand);
+    }
+
+    @Override
+    public int creatBrand(PmsBrand pmsBrand) {
+        int count = pmsBrandMapper.insert(pmsBrand);
+        return count;
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        int count = pmsBrandMapper.deleteByPrimaryKey(id);
+        return count;
+    }
+
+    @Override
+    public PmsBrand selectBrandById(Long id) {
+        PmsBrand pmsBrand = pmsBrandMapper.selectByPrimaryKey(id);
+        return pmsBrand;
+    }
+}
